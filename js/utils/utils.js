252c9fe8c82b866456e17a442a557ebbef9b7f66
ennonn <145817655+ennonn@users.noreply.github.com>
2023-12-26 04:30:31 +0800

    Update utils.js

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -4,7 +4,7 @@ import { setRouter } from "../router/router.js";
 setRouter();
 
 // Backend URL
-const backendURL = "https://98e4-216-247-39-181.ngrok-free.app";
+const backendURL = "https://d2a9-216-247-39-181.ngrok-free.app";
 
 // Get Logged User Profile
 async function getLoggedUser() {
